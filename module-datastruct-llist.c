nickersk/oscam

commit f0bd6056bb954daa1bc48694a059f6b84f718782

Some fixes to the linked list. See http://streamboard.gmc.to/wbb2/thread.php?threadid=32648 for details.
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@5866 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-datastruct-llist.c
@@ -29,21 +29,6 @@ LLIST *ll_create()
     return l;
 }
 
-int32_t ll_lock(LLIST *l)
-{
-	if (l)
-		cs_writelock(&l->lock);
-	return 1;
-}
-
-void ll_unlock(LLIST *l)
-{
-	if (l) {
-		l->version++;
-		cs_writeunlock(&l->lock);
-	}
-}
-
 void ll_destroy(LLIST *l)
 {
     if (!l) return;
@@ -60,43 +45,45 @@ void ll_destroy_data(LLIST *l)
     _destroy(l);
 }
 
+/* Internal iteration function. Make sure that you don't have a lock and that it and it->l are set. */
 static void *ll_iter_next_nolock(LL_ITER *it)
 {
-	if (it && it->l) {
-		if (it->l->version > it->ll_version) {
-			cs_debug_mask_nolock(D_TRACE, "list changed, searching new position");
+	if (it->l->version != it->ll_version) {
+		cs_debug_mask_nolock(D_TRACE, "list changed, searching new position");
 
-			LL_NODE *ptr;
+		LL_NODE *ptr;
+		cs_readlock(&it->l->lock);
+		if (!it->cur && !it->prv) {
+			it->cur = it->l->initial;
+		} else {
+			LL_NODE *prvnxt = NULL;
 			for (ptr = it->l->initial; ptr; ptr = ptr->nxt) {
-				if (!it->cur && !it->prv) {
-					it->cur = ptr;
-					break;
-				}
-
-				if (ptr == it->prv && ptr->nxt != it->cur) {
-					it->cur = ptr->nxt;
-					break;
-				}
+				if (ptr == it->prv && ptr->nxt != it->cur)
+					prvnxt = ptr->nxt;
 				if (ptr == it->cur) {
 					it->prv = ptr;
 					it->cur = ptr->nxt;
 					break;
 				}
 			}
-
-			if (it->cur)
-				return it->cur->obj;
-
-		} else {
-			if (it->cur) {
-				it->prv = it->cur;
-				it->cur = it->cur->nxt;
-			} else if (it->l->initial && !it->prv)
-				it->cur = it->l->initial;
-        
-			if (it->cur)
-				return it->cur->obj;
+			if (!ptr) 
+				it->cur = prvnxt;
 		}
+		it->ll_version = it->l->version;
+		cs_readunlock(&it->l->lock);
+		
+		if (it->cur)
+			return it->cur->obj;
+
+	} else {
+		if (it->cur) {
+			it->prv = it->cur;
+			it->cur = it->cur->nxt;
+		} else if (it->l->initial && !it->prv)
+			it->cur = it->l->initial;
+      
+		if (it->cur)
+			return it->cur->obj;
 	}
 	return NULL;
 }
@@ -105,7 +92,7 @@ static void ll_clear_int(LLIST *l, int32_t clear_data)
 {
     if (!l) return;
 
-    if (!ll_lock(l)) return;
+    cs_writelock(&l->lock);
     
     LL_NODE *n=l->initial, *nxt;
     while (n) {
@@ -117,10 +104,11 @@ static void ll_clear_int(LLIST *l, int32_t clear_data)
 		}
 		n = nxt;
     }
+    l->version++;
     l->count = 0;
     l->initial = 0;
     l->last = 0;
-    ll_unlock(l);
+    cs_writeunlock(&l->lock);
 }
 
 void ll_clear(LLIST *l)
@@ -134,7 +122,8 @@ void ll_clear_data(LLIST *l)
 	ll_clear_int(l, 1);
 }
 
-LL_NODE* ll_append_nolock(LLIST *l, void *obj)
+/* Appends to the list. Do not call this from outside without having a lock! */
+static LL_NODE* ll_append_nolock(LLIST *l, void *obj)
 {
     if (l && obj) {
         LL_NODE *new;
@@ -157,10 +146,10 @@ LL_NODE* ll_append_nolock(LLIST *l, void *obj)
 LL_NODE* ll_append(LLIST *l, void *obj)
 {
     if (l && obj) {
-        if (!ll_lock(l)) return NULL;
+        cs_writelock(&l->lock);
         
         LL_NODE *n = ll_append_nolock(l, obj);
-        ll_unlock(l);
+        cs_writeunlock(&l->lock);
         return n;
     }
     return NULL;
@@ -171,17 +160,17 @@ LL_NODE *ll_prepend(LLIST *l, void *obj)
     if (l && obj) {
         LL_NODE *new;
         if(!cs_malloc(&new,sizeof(LL_NODE), -1)) return NULL;
-
-        if (!ll_lock(l)) { add_garbage(new); return NULL; }
-
-        new->obj = obj;
+				new->obj = obj;
+				
+        cs_writelock(&l->lock);
+        
         new->nxt = l->initial;
 
         l->initial = new;
         if (!l->last)
         	l->last = l->initial;
         l->count++;
-        ll_unlock(l);
+        cs_writeunlock(&l->lock);
 
         return new;
     }
@@ -202,37 +191,31 @@ LL_ITER ll_iter_create(LLIST *l)
 
 void *ll_iter_next(LL_ITER *it)
 {
-    if (it && it->l) {
-		if (!ll_lock(it->l)) return NULL;
-		void *res = ll_iter_next_nolock(it);
-		ll_unlock(it->l);
-		it->ll_version = it->l->version;
-		return res;
-    }
-    return NULL;
+	if (it && it->l) {
+		return ll_iter_next_nolock(it);
+	}
+	return NULL;
 }
 
 void *ll_iter_move(LL_ITER *it, int32_t offset)
 {
-    if (it && it->l) {
-    	if (!ll_lock(it->l)) return NULL;
-    	int32_t i;
-    	void *res = NULL;
-    	for (i=0; i<offset; i++) {
-    		res = ll_iter_next_nolock(it);
-    		if (!res) break;
+	if (it && it->l) {
+		int32_t i;
+		void *res = NULL;
+		for (i=0; i<offset; i++) {
+			res = ll_iter_next_nolock(it);
+			if (!res) break;
+		}
+		
+		return res;
 	}
-	ll_unlock(it->l);
-	it->ll_version = it->l->version;
-	return res;
-    }
-    return NULL;
+	return NULL;
 }
 
 void *ll_iter_peek(LL_ITER *it, int32_t offset)
 {
 	if (it && it->l) {
-		if (!ll_lock(it->l)) return NULL;
+		cs_readlock(&it->l->lock);
 		
 	    LL_NODE *n = it->cur;
 	    int32_t i;
@@ -243,7 +226,7 @@ void *ll_iter_peek(LL_ITER *it, int32_t offset)
 			else 
 				break;
 		}
-		ll_unlock(it->l);
+		cs_readunlock(&it->l->lock);
 	    
 		if (!n)
 			return NULL;
@@ -263,84 +246,109 @@ void ll_iter_reset(LL_ITER *it)
 void ll_iter_insert(LL_ITER *it, void *obj)
 {
     if (it && obj) {
-	   	if (!ll_lock(it->l)) return;
+	   	cs_writelock(&it->l->lock);
 	   	
         if (!it->cur || !it->cur->nxt)
             ll_append_nolock(it->l, obj);
         else {
             LL_NODE *n;
-            if(!cs_malloc(&n,sizeof(LL_NODE), -1)) { ll_unlock(it->l); return; }
+            if(!cs_malloc(&n,sizeof(LL_NODE), -1)) { cs_writeunlock(&it->l->lock); return; }
 
             n->obj = obj;
             n->nxt = it->cur->nxt;
             it->cur->nxt = n;
 
             it->l->count++;
+            it->ll_version = ++it->l->version;
         }
-        ll_unlock(it->l);
+        cs_writeunlock(&it->l->lock);
     }
 }
 
+/* Removes the element to which the iterator currently points. */
 void *ll_iter_remove(LL_ITER *it)
 {
-   	void *obj = NULL;
-    if (it) {
-    	if (!ll_lock(it->l)) return NULL;
-        LL_NODE *del = it->cur;
-        if (del && !del->flag++) { //preventing duplicate free because of multiple threads
-            obj = del->obj;
-            LL_NODE *prv = it->prv;
-            
-            if (prv)
-                prv->nxt = del->nxt;
-            else
-                it->l->initial = del->nxt;
-            if (!it->l->initial)
-            	it->l->last = NULL;
+	void *obj = NULL;
+	if (it) {		
+		LL_NODE *del = it->cur;
+		if (del && !del->flag++) { //preventing duplicate free because of multiple threads
+			obj = del->obj;
+			LL_NODE *prv = it->prv;
+			cs_writelock(&it->l->lock);
+			if(it->ll_version != it->l->version || !prv){		// List has been modified so it->prv might be wrong!
+				LL_NODE *n = it->l->initial;
+				prv = NULL;
+				while(n && n != del){
+					prv = n;
+					n = n->nxt;
+				}
+				if(n != del) return NULL;
+			}
+			
+			if (prv)
+				prv->nxt = del->nxt;
+			else
+				it->l->initial = del->nxt;
+			if (!it->l->initial)
+				it->l->last = NULL;
 			else if (del == it->l->last)
 				it->l->last = prv;
-				
-            it->l->count--;
-            ll_iter_reset(it);
-            while (prv && ll_iter_next_nolock(it))
-                if (it->cur == prv)
-                    break;
+	
+			it->cur = it->l->initial;
+			it->prv = NULL;
+			if(prv != NULL){
+				while(it->cur && it->cur != prv){
+					it->prv = it->cur;
+					it->cur = it->cur->nxt;
+				}
+			}
+			it->l->count--;
+			it->ll_version = ++it->l->version;
+			
+			cs_writeunlock(&it->l->lock);
+			add_garbage(del);
+		}
+	}
 
-            add_garbage(del);
-        }
-        ll_unlock(it->l);
-        it->ll_version = it->l->version;
-    }
-
-    return obj;
+	return obj;
 }
 
+/* Moves the element which is currently pointed to by the iterator to the head of the list.*/
 int32_t ll_iter_move_first(LL_ITER *it) 
 {
 	int32_t moved = 0;
-    if (it) {
-    	if (!ll_lock(it->l)) return moved;
-    	
-        LL_NODE *move = it->cur;
-        if (move && !move->flag++) { //preventing duplicate free because of multiple threads
-            LL_NODE *prv = it->prv;
-            
-            if (prv)
-                prv->nxt = move->nxt;
-            else
-                it->l->initial = move->nxt;
-					        	
+	if (it) {	
+		LL_NODE *move = it->cur;
+		if (move && !move->flag++) { //preventing duplicate free because of multiple threads
+			LL_NODE *prv = it->prv;
+			cs_writelock(&it->l->lock);
+			if(it->ll_version != it->l->version || !prv){		// List has been modified so it->prv might be wrong!
+				LL_NODE *n = it->l->initial;
+				prv = NULL;
+				while(n && n != move){
+					prv = n;
+					n = n->nxt;
+				}
+				if(n != move) return moved;
+			}
+			
+			if (prv)
+				prv->nxt = move->nxt;
+			else
+				it->l->initial = move->nxt;
+			
 			if (prv && it->l->last == move)
 				it->l->last = prv;
 			move->nxt = it->l->initial;
 			it->l->initial = move;
-			moved = 1;
 			
-			ll_iter_reset(it);
-        }
-        ll_unlock(it->l);
-    }
-    return moved;
+			it->ll_version = ++it->l->version;
+			it->prv = NULL;
+			cs_writeunlock(&it->l->lock);
+			moved = 1;
+		}		
+	}
+	return moved;
 }
 
 void ll_iter_remove_data(LL_ITER *it)
@@ -434,34 +442,24 @@ int32_t ll_remove_all(LLIST *l, LLIST *elements_to_remove)
 		return count;
 }
 
-void ll_sort(LLIST *l, void *compare)
+/* Returns an array with all elements sorted, the amount of elements is stored in size. We do not sort the original linked list 
+   as this might harm running iterations. Furthermore, we need the array anyway for qsort() to work. Remember to free() the result. */
+void **ll_sort(LLIST *l, void *compare, int32_t *size)
 {
-	if (!l || !l->initial || !compare) return;
-	
-	if (!ll_lock(l)) return;
-
-	//Because this list has no prv pointer, we can not do qsort, so 
-	//copy to a flat array, sort them, and then copy back
-	void **p = cs_malloc(&p, l->count*sizeof(p[0]), 0);
-	LL_NODE *n = l->initial;
+	if (!l || !l->initial || !compare) return NULL;
 	int32_t i=0;
-	while (n) {
-		p[i++] = n->obj;
-		n=n->nxt;
-	}
+	LL_NODE *n;
 	
-	cs_debug_mask(D_TRACE, "sort: count %d size %d", l->count, sizeof(p[0]));
+	cs_readlock(&l->lock);
+	*size = l->count;
+	void **p = cs_malloc(&p, l->count*sizeof(p[0]), 0);	
+	for (n = l->initial; n; n = n->nxt) {
+		p[i++] = n->obj;
+	}
+	cs_readunlock(&l->lock);
+	//cs_debug_mask(D_TRACE, "sort: count %d size %d", l->count, sizeof(p[0]));
 	
 	qsort(p, l->count, sizeof(p[0]), compare);
-	
-	n = l->initial;
-	i = 0;
-	while (n) {
-		n->obj = p[i++];
-		n=n->nxt;
-	}
-	
-	free(p);
-	
-	ll_unlock(l);
+
+	return p;
 }

global-functions.h
@@ -414,7 +414,7 @@ extern READER_STAT *get_stat(struct s_reader *rdr, uint16_t caid, uint32_t prid,
 extern void save_stat_to_file(int32_t);
 extern void clear_all_stat();
 extern void housekeeping_stat(int32_t force);
-extern void sort_stat(struct s_reader *rdr, int32_t reverse);
+extern READER_STAT **get_sorted_stat_copy(struct s_reader *rdr, int32_t reverse, int32_t *size);
 extern int32_t clean_stat_by_rc(struct s_reader *rdr, int8_t rc);
 extern int32_t clean_stat_by_id(struct s_reader *rdr, uint32_t caid, uint32_t provid, uint32_t sid, uint32_t len);
 #endif

module-cccshare.c
@@ -1222,12 +1222,12 @@ int32_t compare_cards_by_hop_r(struct cc_card **pcard1, struct cc_card **pcard2)
 	return -compare_cards_by_hop(pcard1, pcard2);
 }
 
-void sort_cards_by_hop(LLIST *cards, int32_t reverse)
+struct cc_card **get_sorted_card_copy(LLIST *cards, int32_t reverse, int32_t *size)
 {
 	if (reverse)
-		ll_sort(cards, compare_cards_by_hop_r);
+		return (struct cc_card **)ll_sort(cards, compare_cards_by_hop_r, size);
 	else
-		ll_sort(cards, compare_cards_by_hop);
+		return (struct cc_card **)ll_sort(cards, compare_cards_by_hop, size);
 }
 
 void init_share() {

module-cccshare.h
@@ -39,6 +39,6 @@ void send_remove_card_to_clients(struct cc_card *card);
 
 int32_t cc_srv_report_cards(struct s_client *cl);
 
-void sort_cards_by_hop(LLIST *cards, int32_t reverse);
+struct cc_card **get_sorted_card_copy(LLIST *cards, int32_t reverse, int32_t *size);
 #endif /* MODULECCCSHARE_H_ */
 #endif

module-datastruct-llist.h
@@ -19,7 +19,7 @@ struct llist {
     int32_t count;
     CS_MUTEX_LOCK lock;
     int32_t flag;
-    uint32_t version;
+    uint32_t version;		// updated on every modification of the list - exception is on appends as this should not have impacts on iterations!
 };
 
 typedef struct lliter LL_ITER;
@@ -35,7 +35,7 @@ void ll_destroy_data(LLIST *l); // same as ll_clear_data() but frees up obj allo
 void ll_clear(LLIST *l);        // frees up all llnodes nodes but not data held in obj ptrs
 void ll_clear_data(LLIST *l);   // same as ll_clear_data() but frees up obj allocations as well
 
-void ll_sort(LLIST *l, void *compare); // sorts the list, compare = int func(const T *a, const T *b)
+void **ll_sort(LLIST *l, void *compare, int32_t *size); // sorts the list, compare = int func(const T *a, const T *b)
 LL_NODE *ll_append(LLIST *l, void *obj);                // append obj to llist
 LL_NODE *ll_prepend(LLIST *l, void *obj);               // prepend obj to llist
 

module-stat.c
@@ -1150,12 +1150,12 @@ static int compare_stat_r(READER_STAT **ps1, READER_STAT **ps2) {
 	return -compare_stat(ps1, ps2);
 }
 
-void sort_stat(struct s_reader *rdr, int32_t reverse)
+READER_STAT **get_sorted_stat_copy(struct s_reader *rdr, int32_t reverse, int32_t *size)
 {
 	if (reverse)
-		ll_sort(rdr->lb_stat, compare_stat_r);
+		return (READER_STAT **)ll_sort(rdr->lb_stat, compare_stat_r, size);
 	else
-		ll_sort(rdr->lb_stat, compare_stat);
+		return (READER_STAT **)ll_sort(rdr->lb_stat, compare_stat, size);
 }
 
 #endif

oscam-http.c
@@ -1535,15 +1535,12 @@ static char *send_oscam_reader_stats(struct templatevars *vars, struct uriparams
 	time_t lastaccess = 0;
 
 	if (rdr->lb_stat) {
-	
+		int32_t statsize;
 		// @todo alno: sort by click, 0=ascending, 1=descending (maybe two buttons or reverse on second click)
-		sort_stat(rdr, 0);
-		
-		LL_ITER it = ll_iter_create(rdr->lb_stat);
-		READER_STAT *stat = ll_iter_next(&it);
+		READER_STAT **statarray = get_sorted_stat_copy(rdr, 0, &statsize);
 		char channame[32];
-		while (stat) {
-
+		for(; rowcount < statsize; ++rowcount){
+			READER_STAT *stat = statarray[rowcount];
 			if (!(stat->rc == rc2hide)) {
 				struct tm lt;
 				localtime_r(&stat->last_received, &lt);
@@ -1610,11 +1607,8 @@ static char *send_oscam_reader_stats(struct templatevars *vars, struct uriparams
 					tpl_addVar(vars, TPLAPPEND, "ECMSTATS", tpl_getTpl(vars, "APIREADERSTATSECMBIT"));
 				}
 			}
-
-		stat = ll_iter_next(&it);
-		rowcount++;
 		}
-
+		free(statarray);
 	} else {
 		tpl_addVar(vars, TPLAPPEND, "READERSTATSROW","<TR><TD colspan=\"8\"> No statistics found </TD></TR>");
 	}
@@ -2173,12 +2167,6 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 #define ENTITLEMENT_PAGE_SIZE 500
 
 static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams *params, int32_t apicall) {
-
-	//just to stop the guys open tedious tickets for warnings related to unused variables xD
-	tpl_printf(vars, TPLADD, "ISAPICALL", "%d", apicall);
-	//**************
-
-	/* build entitlements from reader init history */
 	char *reader_ = getParam(params, "label");
 #ifdef MODULE_CCCAM	
 	char *sharelist_ = getParam(params, "globallist");
@@ -2228,19 +2216,18 @@ static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams 
 			if (cards) {
 
 				uint8_t serbuf[8];
+				int32_t cardsize, i, count = 0;
 				char provname[83];
 
 				// @todo alno: sort by click, 0=ascending, 1=descending (maybe two buttons or reverse on second click)
-				sort_cards_by_hop(cards, 0);
-				
-                LL_ITER it = ll_iter_create(cards);
-                int32_t offset2 = offset+1;
-                int32_t count = 0;
-                while ((card = ll_iter_move(&it, offset2))) {
-                	offset2 = 1;
-                	if (count == ENTITLEMENT_PAGE_SIZE)
-                		break;
-                	count++;
+				struct cc_card **cardarray = get_sorted_card_copy(cards, 0, &cardsize);
+					
+					for(i = offset; i < cardsize; ++i) {
+					card = cardarray[i];
+
+					if (count == ENTITLEMENT_PAGE_SIZE)
+						break;
+					count++;
                 	
 					if (!apicall) {
 						if (show_global_list)
@@ -2354,6 +2341,7 @@ static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams 
 
 					cardcount++;
 				}
+				free(cardarray);
 				
 				// set previous Link if needed
 				if (offset >= ENTITLEMENT_PAGE_SIZE) {
@@ -2364,7 +2352,7 @@ static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams 
 				}
 
 				// set next link if needed
-				if (card) {
+				if (cardsize > count && offset < cardsize) {
 					tpl_printf(vars, TPLAPPEND, "CONTROLS", "<A HREF=\"entitlements.html?offset=%d&globallist=%s&amp;label=%s\"> > NEXT >> </A>",
 							offset + ENTITLEMENT_PAGE_SIZE,
 							getParam(params, "globallist"),
@@ -2372,10 +2360,10 @@ static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams 
 				}
 
 				if (!apicall) {
-					tpl_printf(vars, TPLADD, "TOTALS", "card count=%d", cardcount);
+					tpl_printf(vars, TPLADD, "TOTALS", "card count=%d", cardsize);
 					tpl_addVar(vars, TPLADD, "ENTITLEMENTCONTENT", tpl_getTpl(vars, "ENTITLEMENTCCCAMBIT"));
 				} else {
-					tpl_printf(vars, TPLADD, "APITOTALCARDS", "%d", cardcount);
+					tpl_printf(vars, TPLADD, "APITOTALCARDS", "%d", cardsize);
 				}
 
 			} else {

oscam.c
@@ -3122,9 +3122,12 @@ void * work_thread(void *ptr) {
 		data = NULL;
 	}
 
-	if (!keep_threads_alive && thread_pipe[1])
-		write(thread_pipe[1], mbuf, 1); //wakeup client check
-
+	if (!keep_threads_alive && thread_pipe[1]){
+		if(write(thread_pipe[1], mbuf, 1) == -1){ //wakeup client check
+			cs_debug_mask(D_TRACE, "Writing to pipe failed (errno=%d %s)", errno, strerror(errno));
+		}
+	}
+	
 	cs_debug_mask(D_TRACE, "ending thread");
 
 	pthread_exit(NULL);
@@ -3364,7 +3367,9 @@ void * client_check(void) {
 
 			if (pfd[i].fd == thread_pipe[0] && (pfd[i].revents & (POLLIN | POLLPRI))) {
 				// a thread ended and cl->pfd should be added to pollfd list again (thread_active==0)
-				read(thread_pipe[0], buf, sizeof(buf));
+				if(read(thread_pipe[0], buf, sizeof(buf)) == -1){
+					cs_debug_mask(D_TRACE, "Reading from pipe failed (errno=%d %s)", errno, strerror(errno));
+				}
 				continue;
 			}
 

commit 0742fc7f78e3e19f11c7ea7097b8f4f288b06c58

Fix for missing cards caused by r5866.
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@5896 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-datastruct-llist.c
@@ -304,7 +304,7 @@ void *ll_iter_remove(LL_ITER *it)
 					it->prv = it->cur;
 					it->cur = it->cur->nxt;
 				}
-			}
+			} else it->cur = NULL;
 			it->l->count--;
 			it->ll_version = ++it->l->version;
 			